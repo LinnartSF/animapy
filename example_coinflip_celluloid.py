2bda24233adf78d6f9888f93271108662c673d73
LinnartSF <linnart.felkl@hotmail.com>
2022-10-21 18:44:56 +0200

    .

---
+++ example_coinflip_celluloid.py
@@ -0,0 +1,31 @@
+from matplotlib import pyplot as plt
+from celluloid import Camera
+import random
+
+# create figure object
+fig = plt.figure()
+
+# specify labels
+plt.xlabel("coin side")
+plt.ylabel("relative frequency")
+plt.title("heads or tail frequency")
+
+# generate animation data
+tailheads = [0,0]
+xaxpos = [1,2]
+camera = Camera(fig)
+plt.xticks(xaxpos, labels=["tails","heads"])
+
+for i in range(1000):
+    fig.clf()
+    if random.randint(0,1) > 0:
+        tailheads[1] += tailheads[1]
+    else:
+        tailheads[0] += tailheads[0]
+    plt.bar(xaxpos, tailheads)
+    plt.pause(0.1)
+    camera.snap()
+
+# build animation from data and save it
+animation = camera.animate()
+animation.save('animations/scatteranimation_celluloid.gif', writer='PillowWriter', fps=2)
