5f3d9cd4b560b0bb0e399bdcf78a963bbaf74c2f
LinnartSF <linnart.felkl@hotmail.com>
2022-10-21 17:15:29 +0200

    implemented celluloid

--- example_scatterplotting_celluloid.py
+++ example_scatterplotting_celluloid.py
@@ -1,19 +1,21 @@
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
+from celluloid import Camera
 import numpy as np
-from sklearn.linear_model import LinearRegression
-import random
 
-x_values = []
-y_values = []
 
-for _ in range(1000):
-    x_values.append(random.randint(0, 100))
-    y_values.append(random.randint(0, 100))
+# create figure object
+fig = plt.figure()
+# load axis box
+ax = plt.axes()
+# set axis limit
+ax.set_ylim(0, 1)
+ax.set_xlim(0, 10)
 
-    plt.xlim(0,100)
-    plt.ylim(0,100)
-    plt.scatter(x = x_values, y = y_values, color = "black")
-    
-    plt.pause(0.001)
+camera = Camera(fig)
+for i in range(10):
+    ax.scatter(i, np.random.random())
+    plt.pause(0.1)
+    camera.snap()
 
-plt.show()
+animation = camera.animate()
+animation.save('animation.gif', writer='PillowWriter', fps=2)
