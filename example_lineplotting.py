0f7d689900c7161a540a2ce8cc61822d5f48a1f2
LinnartSF <linnart.felkl@hotmail.com>
2022-10-21 15:34:39 +0200

    .

---
+++ example_lineplotting.py
@@ -0,0 +1,20 @@
+from matplotlib import pyplot as plt
+from celluloid import Camera
+import numpy as np
+
+# create figure object
+fig = plt.figure()
+# load axis box
+ax = plt.axes()
+# set axis limit
+ax.set_ylim(0, 1)
+ax.set_xlim(0, 10)
+
+camera = Camera(fig)
+for i in range(10):
+    ax.scatter(i, np.random.random())
+    plt.pause(0.1)
+    camera.snap()
+
+animation = camera.animate()
+animation.save('animations/scatteranimation_celluloid.gif', writer='PillowWriter', fps=2)
