7d6243c9d8175d7e841191dfe139dbc487ed2646
LinnartSF <linnart.felkl@hotmail.com>
2022-10-21 18:51:26 +0200

    .

--- example_coinflip_celluloid.py
+++ example_coinflip_celluloid.py
@@ -13,18 +13,23 @@ plt.title("heads or tail frequency")
 # generate animation data
 tailheads = [0,0]
 xaxpos = [1,2]
+
 camera = Camera(fig)
+
 plt.xticks(xaxpos, labels=["tails","heads"])
 
-for i in range(50):
+for i in range(1000):
+    
     if random.randint(0,1) > 0:
-        tailheads[1] += tailheads[1]
+        tailheads[1] = tailheads[1] + 1
     else:
-        tailheads[0] += tailheads[0]
+        tailheads[0] = tailheads[0] + 1
+
     plt.bar(xaxpos, tailheads)
     plt.pause(0.1)
+    
     camera.snap()
 
 # build animation from data and save it
 animation = camera.animate()
-animation.save('animations/scatteranimation_celluloid.gif', writer='PillowWriter', fps=20)
+animation.save('animations/coinflipanimation_celluloid.gif', writer='PillowWriter', fps=300)
