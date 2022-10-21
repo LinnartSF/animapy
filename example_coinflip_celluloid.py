48018226b079717213673fff398eb52e6fedc011
LinnartSF <linnart.felkl@hotmail.com>
2022-10-21 19:04:30 +0200

    .

--- example_coinflip_celluloid.py
+++ example_coinflip_celluloid.py
@@ -7,7 +7,7 @@ fig = plt.figure()
 
 # specify labels
 plt.xlabel("coin side")
-plt.ylabel("relative frequency")
+plt.ylabel("absolute frequency")
 plt.title("heads or tail frequency")
 
 # generate animation data
@@ -18,18 +18,18 @@ camera = Camera(fig)
 
 plt.xticks(xaxpos, labels=["tails","heads"])
 
-for i in range(1000):
+for i in range(100):
     
     if random.randint(0,1) > 0:
         tailheads[1] = tailheads[1] + 1
     else:
         tailheads[0] = tailheads[0] + 1
 
-    plt.bar(xaxpos, tailheads, color = "blue")
+    plt.bar(xaxpos, tailheads, color = ["darkgreen","lightgreen"])
     plt.pause(0.005)
     
     camera.snap()
 
 # build animation from data and save it
 animation = camera.animate()
-animation.save('animations/coinflipanimation_celluloid.gif', writer='PillowWriter', fps=200)
+animation.save('animations/coinflipanimation_celluloid.gif', writer='PillowWriter', fps=10)
